d083afd0532ed6386b40a118b19dda557bca60a1
Samuel Onyijne <samuelord1@gmail.com>
2020-04-11 19:14:10 +0100

    fixed undefined

--- src/severeImpact.js
+++ src/severeImpact.js
@@ -1,5 +1,5 @@
 const severeImpact = (data) => ({
   currentlyInfected: Math.trunc(data.reportedCases * 50),
-  infectionsByRequestedTime: Math.trunc(this.currentlyInfected * 1024)
+  infectionsByRequestedTime: Math.trunc(severeImpact.currentlyInfected * 1024)
 });
 export default severeImpact;
