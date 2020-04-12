508e40914cbc289400748bad0cb3e9431d1f83f9
Samuel Onyijne <samuelord1@gmail.com>
2020-04-12 14:48:36 +0100

    challenge three

--- src/impact.js
+++ src/impact.js
@@ -2,7 +2,7 @@ import periodNormalizer from './periodNormalizer';
 
 const impact = (data) => {
   const {
-    reportedCases, periodType, timeToElapse, totalHospitalBeds
+    reportedCases, periodType, timeToElapse, totalHospitalBeds, region
   } = data;
   const currentlyInfected = reportedCases * 10;
   const elapse = periodNormalizer(periodType, timeToElapse);
@@ -10,11 +10,18 @@ const impact = (data) => {
   const severeCasesByRequestedTime = Math.trunc((15 / 100) * infectionsByRequestedTime);
   const hospitalBedsByRequestedTime = Math.trunc(((35 / 100) * totalHospitalBeds)
   - severeCasesByRequestedTime);
+  const casesForICUByRequestedTime = Math.trunc((5 / 100) * infectionsByRequestedTime);
+  const casesForVentilatorsByRequestedTime = Math.trunc((2 / 100) * infectionsByRequestedTime);
+  const dollarsInFlight = Math.trunc((infectionsByRequestedTime * region.avgDailyIncomePopulation)
+  * region.avgDailyIncomeInUSD * 30);
   return {
     currentlyInfected,
     infectionsByRequestedTime,
     severeCasesByRequestedTime,
-    hospitalBedsByRequestedTime
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
   };
 };
 export default impact;
