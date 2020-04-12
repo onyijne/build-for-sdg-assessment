111851f01334658ae3a302db159a14e690859701
Samuel Onyijne <samuelord1@gmail.com>
2020-04-12 13:49:22 +0100

    updated

--- src/impact.js
+++ src/impact.js
@@ -8,8 +8,8 @@ const impact = (data) => {
   const elapse = periodNormalizer(periodType, timeToElapse);
   const infectionsByRequestedTime = currentlyInfected * (2 ** elapse);
   const severeCasesByRequestedTime = Math.trunc((15 / 100) * infectionsByRequestedTime);
-  const hospitalBedsByRequestedTime = Math.trunc((35 / 100) * totalHospitalBeds)
-  - severeCasesByRequestedTime;
+  const hospitalBedsByRequestedTime = Math.trunc(((35 / 100) * totalHospitalBeds)
+  - severeCasesByRequestedTime);
   return {
     currentlyInfected,
     infectionsByRequestedTime,
