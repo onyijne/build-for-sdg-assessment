1b22ee6bee268a860d394c9722f51efa4aa15baf
Samuel Onyijne <samuelord1@gmail.com>
2020-04-12 15:05:46 +0100

    updated normalizer

--- src/periodNormalizer.js
+++ src/periodNormalizer.js
@@ -1,17 +1,24 @@
-const periodNormalizer = (periodType, timeToElapse) => {
+const periodNormalizer = (periodType, timeToElapse, avgDailyIncomeInUSD) => {
   let elapse;
+  let amount;
   switch (periodType.toLowerCase()) {
     case 'months':
       elapse = (timeToElapse / 3) * 30;
+      amount = avgDailyIncomeInUSD * 30;
       break;
     case 'weeks':
       elapse = (timeToElapse / 3) * 7;
+      amount = avgDailyIncomeInUSD * 7;
       break;
     default:
       elapse = (timeToElapse / 3);
+      amount = avgDailyIncomeInUSD;
       break;
   }
-  return Math.trunc(elapse);
+  return {
+    elapseTime: Math.trunc(elapse),
+    avgDailyIncome: amount
+  };
 };
 
 export default periodNormalizer;
