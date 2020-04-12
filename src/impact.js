48ec784751d61fa494731b3d2f91a28d6a88a13c
Samuel Onyijne <samuelord1@gmail.com>
2020-04-12 08:46:50 +0100

    separated period normalizer

--- src/impact.js
+++ src/impact.js
@@ -1,18 +1,9 @@
+import periodNormalizer from './periodNormalizer';
+
 const impact = (data) => {
   const { reportedCases, periodType, timeToElapse } = data;
   const currentlyInfected = reportedCases * 10;
-  let elapse;
-  switch (periodType.toLowerCase()) {
-    case 'months':
-      elapse = Math.trunc(timeToElapse / 3) * 30;
-      break;
-    case 'weeks':
-      elapse = Math.trunc(timeToElapse / 3) * 7;
-      break;
-    default:
-      elapse = Math.trunc(timeToElapse / 3);
-      break;
-  }
+  const elapse = periodNormalizer(periodType, timeToElapse);
   const infectionsByRequestedTime = currentlyInfected * (2 ** elapse);
   return {
     currentlyInfected,
--- src/severeImpact.js
+++ src/severeImpact.js
@@ -1,18 +1,9 @@
+import periodNormalizer from './periodNormalizer';
+
 const severeImpact = (data) => {
   const { reportedCases, periodType, timeToElapse } = data;
   const currentlyInfected = reportedCases * 50;
-  let elapse;
-  switch (periodType.toLowerCase()) {
-    case 'months':
-      elapse = Math.trunc(timeToElapse / 3) * 30;
-      break;
-    case 'weeks':
-      elapse = Math.trunc(timeToElapse / 3) * 7;
-      break;
-    default:
-      elapse = Math.trunc(timeToElapse / 3);
-      break;
-  }
+  const elapse = periodNormalizer(periodType, timeToElapse);
   const infectionsByRequestedTime = currentlyInfected * (2 ** elapse);
   return {
     currentlyInfected,
