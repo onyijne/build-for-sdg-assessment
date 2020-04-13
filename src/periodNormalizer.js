b9563b4f0c29736a8e638c5ff8a027a9272d9fe6
Samuel Onyijne <samuelord1@gmail.com>
2020-04-13 14:31:36 +0100

    updated

--- src/periodNormalizer.js
+++ src/periodNormalizer.js
@@ -1,23 +1,19 @@
 const periodNormalizer = (periodType, timeToElapse) => {
   let elapse;
-  let amount;
   switch (periodType.toLowerCase()) {
     case 'months':
       elapse = (timeToElapse / 3) * 30;
-      amount = (timeToElapse / 3) * 30;
       break;
     case 'weeks':
       elapse = (timeToElapse / 3) * 7;
-      amount = (timeToElapse / 3) * 30;
       break;
     default:
       elapse = (timeToElapse / 3);
-      amount = timeToElapse;
       break;
   }
   return {
     elapseTime: Math.trunc(elapse),
-    dollarElapseTime: amount
+    dollarElapseTime: elapse
   };
 };
 
