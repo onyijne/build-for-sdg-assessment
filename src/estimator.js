6b8513967ef8c772325a7a1db11ef87819df2969
Samuel Onyijne <samuelord1@gmail.com>
2020-04-13 14:55:08 +0100

    updated time

--- src/estimator.js
+++ src/estimator.js
@@ -1,20 +1,6 @@
 import impact from './impact';
 import severeImpact from './severeImpact';
 
-/* const data = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-}; */
-
 const covid19ImpactEstimator = (data) => ({
   data,
   impact: impact(data),
--- src/periodNormalizer.js
+++ src/periodNormalizer.js
@@ -1,19 +1,23 @@
 const periodNormalizer = (periodType, timeToElapse) => {
   let elapse;
+  let dollarElapse;
   switch (periodType.toLowerCase()) {
     case 'months':
       elapse = (timeToElapse / 3) * 30;
+      dollarElapse = timeToElapse * 30;
       break;
     case 'weeks':
       elapse = (timeToElapse / 3) * 7;
+      dollarElapse = timeToElapse * 7;
       break;
     default:
       elapse = (timeToElapse / 3);
+      dollarElapse = timeToElapse;
       break;
   }
   return {
     elapseTime: Math.trunc(elapse),
-    dollarElapseTime: elapse
+    dollarElapseTime: dollarElapse
   };
 };
 
